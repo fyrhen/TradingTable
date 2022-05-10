fa45a8275d5e0b31b33b063160b9ddfad7c21377
Juliano Grobério <julianogroberio@gmail.com>
2022-05-10 13:54:37 -0300

    Surpresa ao instanciar uma negociação

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,8 @@
-import { Negociacao } from "./models/negociacao.js";
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao.volume);
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    controller.adiciona();
+    event.preventDefault();
+    controller.adiciona();
+});
