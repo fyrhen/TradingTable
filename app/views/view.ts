c0665fe30f315372b59872eec757b4ff2b893cc7
Juliano Grobério <julianogroberio@gmail.com>
2022-06-21 12:00:45 -0300

    Lapidando nosso código

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,12 +1,19 @@
 export abstract class View<T> {
     protected elemento: HTMLElement;
+    private escapar = false;
 
-    constructor(selector: string) {
+    constructor(selector: string, escapar?: boolean) {
         this.elemento = document.querySelector(selector);
+        if (escapar) {
+            this.escapar = escapar;
+        }
     }
 
     update(model: T): void {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
         this.elemento.innerHTML = template;
     }
 
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,9 +1,16 @@
 export class View {
-    constructor(selector) {
+    constructor(selector, escapar) {
+        this.escapar = false;
         this.elemento = document.querySelector(selector);
+        if (escapar) {
+            this.escapar = escapar;
+        }
     }
     update(model) {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
         this.elemento.innerHTML = template;
     }
 }
